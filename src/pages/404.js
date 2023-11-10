f93fd54807c4a3171223f77eacc5f149699b0709
Yeadam Kim <yes752044@gmail.com>
2023-11-10 01:18:51 -0800

    v1.1.2 fixed path 404.js

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,7 +1,7 @@
 import React from "react";
-import Layout from "../components/layout";
-import Seo from "../components/seo";
-import "../styles/pages/index.scss";
+import Layout from "/src/components/layout";
+import Seo from "/src/components/seo";
+import "/src/styles/pages/index.scss";
 
 const NotFoundPage = () => (
   <Layout>
