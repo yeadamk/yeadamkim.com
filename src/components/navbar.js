f22f2e477b522b97dc6d1105960c5c121db70aae
Yeadam Kim <yes752044@gmail.com>
2024-03-28 02:36:44 -0700

    Fix cursor pointer not showing over the hamburger
    The previous commit didn't quite fix it

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -154,8 +154,8 @@ const Navbar = () => {
           />
         </LogoWrap>
         <Toggle
-          onClick={() => setNavbarOpen(!navbarOpen)}
           className="clickable"
+          onClick={() => setNavbarOpen(!navbarOpen)}
         >
           {navbarOpen ? (
             <Hamburger open className="clickable" />
